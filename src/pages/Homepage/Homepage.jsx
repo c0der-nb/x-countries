e4e4fd19b295775d55e5084f8bc2a64b8741890a
Aditya Gupta <gaditya060@gmail.com>
2024-03-26 00:52:04 +0530

    Handle API error

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,28 +1,32 @@
 import React, {useState, useEffect, lazy, Suspense} from "react";
 import styles from "./Homepage.module.css";
-import { fetchCountryFlags } from "../../api/api";
+import axios from "axios";
 const Flag = lazy(() => import("../../components/Flag/Flag"));
 
 export default function Homepage() {
     const [flagData, setFlagData] = useState([]);
 
-    const generateData = (source) => {
-        source().then((data) => {
-            setFlagData(data);
-        }).catch((err) => console.error(err));
+    const generateData = async () => {
+       try {
+        const apiResponse = await axios.get("https://restcountries.com/v3.1/all");
+        setFlagData(apiResponse.data);
+       }
+       catch (ex) {
+        console.error(ex);
+       }
     }
 
     useEffect(() => {
-        generateData(fetchCountryFlags);
+        generateData();
     }, []);
 
     return (
         <div className={styles.container}>
-            {flagData.map((data) => (
             <Suspense fallback={<div>Loading...</div>}>
-                <Flag image={data.flags.svg} name={data.name.common} alt={data.flags.alt} />
-            </Suspense>
+            {flagData.map((data) => (
+                <Flag image={data.flags.svg} name={data.name.common} alt={data.flags.alt} key={data.name.official} />
             ))}
+            </Suspense>
         </div>
     )
 }
