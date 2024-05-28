8784d78f7244a21e068bf70526f55c5b6574c5c1
Aditya Gupta <gaditya060@gmail.com>
2024-05-29 01:08:15 +0530

    Add search functionality

--- src/components/Flag/Flag.jsx
+++ src/components/Flag/Flag.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import styles from "./Flag.module.css";
+import "./Flag.module.css";
 
 export default function Flag({image, name, alt}) {
     return (
-        <div className={styles.card}>
-            <div>
-                <img className={styles.flagImage} src={image} alt={alt} />
-            </div>
+            <div className="countryCard">
+                <img className="flagImage" src={image} alt={alt} />
+            
             <p>{name}</p>
-        </div>
+            </div>
     )
 }
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,19 +1,29 @@
 import React, {useState, useEffect, lazy, Suspense} from "react";
 import styles from "./Homepage.module.css";
 import axios from "axios";
-const Flag = lazy(() => import("../../components/Flag/Flag"));
+import Flag from "../../components/Flag/Flag"
 
 export default function Homepage() {
     const [flagData, setFlagData] = useState([]);
+    const [filteredFlagData, setFilteredFlagData] = useState([]);
+    const [isLoading, setLoading] = useState(true);
 
     const generateData = async () => {
        try {
+        setLoading(true);
         const apiResponse = await axios.get("https://restcountries.com/v3.1/all");
         setFlagData(apiResponse.data);
+        setFilteredFlagData(apiResponse.data);
+        setLoading(false);
        }
        catch (ex) {
+        setLoading(false);
         console.error(ex);
        }
+    }
+
+    const searchHandler = (e) => {
+        setFilteredFlagData((prevVal) => flagData.filter((country) => country.name.common.toLowerCase().includes(e.target.value.toLowerCase()) === true))
     }
 
     useEffect(() => {
@@ -21,12 +31,18 @@ export default function Homepage() {
     }, []);
 
     return (
-        <div className={styles.container}>
-            <Suspense fallback={<div>Loading...</div>}>
-            {flagData.map((data) => (
-                <Flag image={data.flags.svg} name={data.name.common} alt={data.flags.alt} key={data.name.official} />
-            ))}
-            </Suspense>
+        <>
+        <header>
+            <input onChange={searchHandler} className={styles.countrySearch} type="text" name="search" id="country-search" placeholder="Search for countries..." />
+        </header>
+        <div className={styles.wrapper}>
+            {!isLoading ?
+            <div className={styles.container}>
+                {filteredFlagData.map((data) => (
+                    <Flag image={data.flags.svg} name={data.name.common} alt={data.flags.alt} key={data.name.official} />
+                ))}
+            </div> : <div className="loader"></div> }
         </div>
+        </>
     )
 }
